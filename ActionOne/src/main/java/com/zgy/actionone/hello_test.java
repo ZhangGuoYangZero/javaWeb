e4e467b031419ed86a346bda49486f37d76089c4
张国杨Zero <101501786+ZhangGuoYangZero@users.noreply.github.com>
2022-07-15 08:00:55 +0800

    1
    1

---
+++ ActionOne/src/main/java/com/zgy/actionone/hello_test.java
@@ -0,0 +1,13 @@
+package com.zgy.actionone;
+
+/**
+ * @Description:
+ * @Date:2022/7/15 - 7:52
+ */
+public class hello_test {
+
+    @Test
+    public  void test(){
+        System.out.println("hellow_Test自正在");
+    }
+}
